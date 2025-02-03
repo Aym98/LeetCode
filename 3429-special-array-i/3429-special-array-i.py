fe0f51a1f9207a9606f41bbcd38bd10da3c9af1c
Aym98 <105650016+Aym98@users.noreply.github.com>
2025-02-03 13:22:38 +0100

    Time: 52 ms (1.37%), Space: 29.5 MB (10.04%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.py
+++ 3429-special-array-i/3429-special-array-i.py
@@ -1,8 +1,10 @@
+import numpy as np 
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        is_special = True
-        for i in range(len(nums)-1):
-            if (nums[i] + nums[i+1]) % 2 == 0:
-                return False
-        return True
-        
+        n = len(nums)
+        nums_one = nums[0:n-1]
+        nums_two = nums[1:n]
+        final = np.add(nums_one, nums_two)
+        print(final)
+        res = all(final[idx] % 2 != 0 for idx in range(0, n-1))
+        return res
